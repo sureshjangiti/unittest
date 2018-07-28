2eef82d83c87ab38c23cca2aa78b438f6af64d1a
suresh <jangitisuresh@gmail.com>
2018-07-28 10:40:43 +0530

    refactored test code

--- IdeaProjects/example2/src/test/java/UnittestingOfTest.java
+++ IdeaProjects/example2/src/test/java/UnittestingOfTest.java
@@ -2,26 +2,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UnittestingOfTest {
+    Test1 test = new Test1();
     @Test
     public void testingAdditionMethodSuccess(){
-        Test1 test = new Test1();
         Assert.assertEquals(7,test.add(3,4));
-        Assert.assertEquals(6,test.add(2,4));
     }
     @Test
     public void testingAdditionMethodFailure(){
-        Test1 test = new Test1();
         Assert.assertNotEquals(6,test.add(3,4));
     }
-
     @Test
     public void testingIsEvenMethodSuccess(){
-        Test1 test = new Test1();
         Assert.assertTrue(test.iseven(2));
     }
     @Test
     public void testingIsEvenMethodFailure(){
-        Test1 test = new Test1();
         Assert.assertFalse(test.iseven(3));
     }
 }
