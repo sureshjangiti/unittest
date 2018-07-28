ceb43e698b7ba673c6bac80d78324a7d215e4625
suresh <jangitisuresh@gmail.com>
2018-07-28 10:32:15 +0530

    Unittesting and folder creation

---
+++ IdeaProjects/example2/src/main/java/Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+    public int add(int a, int b){
+       return a+b;
+    }
+    public boolean iseven(int val){
+        if(val%2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
